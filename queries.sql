449ad5baa14c7437acb6ef1a7fdbc072aa70f3f8
Victoria Tiller <vijotiller@gmail.com>
2021-04-27 14:40:27 -0400

    tried Epic

--- queries.sql
+++ queries.sql
@@ -51,3 +51,31 @@ CREATE TABLE "Employees" (
  "ParkingSpot"    VARCHAR(10)
  );
  ALTER TABLE "Employees" ADD COLUMN "Id" SERIAL PRIMARY KEY;
+
+
+
+ Epic Mode
+
+ CREATE TABLE "Departments" (
+ "Name"             TEXT NOT NULL,
+ "Building Number"  INT,
+ "ID"               SERIAL PRIMARY KEY
+ );
+
+ALTER TABLE "Employees" ADD COLUMN "DepartmentId" INTEGER NULL REFERENCES "Departments" ("ID");
+
+INSERT INTO "Departments" ("Name", "Building Number")
+ Values ('Florence', 1);
+INSERT INTO "Departments" ("Name", "Building Number")
+ Values ('Venice', 10);
+INSERT INTO "Departments" ("Name", "Building Number")
+ Values ('Ever After', 3);
+INSERT INTO "Departments" ("Name", "Building Number")
+ Values ('Neverland', 7);
+INSERT INTO "Departments" ("Name", "Building Number")
+ Values ('Wonderland', 12);
+
+UPDATE "Employees" SET "DepartmentId" = 3 Where "Id" in (1,2,3,5,8,9);
+UPDATE "Employees" SET "DepartmentId" = 5  Where "Id" in (4);
+UPDATE "Employees" SET "DepartmentId" = 1  Where "Id" in (6, 7);
+UPDATE "Employees" SET "DepartmentId" = 2  Where "Id" in (10);
